3fa9928beab1f0d9727037208d82264db2338cc2
Paul Raffer <paul.raffer@protonmail.com>
2020-12-16 18:08:54 +0100

    create 'lib*.a' files

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 mkdir build
 cd build
-gcc -c ../include/error.c
-gcc -c ../include/str.c
-gcc -c ../include/file.c
-gcc -c ../include/process.c
 
+gcc -c ../include/error.c
+ar rcs liberror.a error.o
+
+gcc -c ../include/str.c
+ar rcs libstr.a str.o
+
+gcc -c ../include/file.c
+ar rcs libfile.a file.o
+
+gcc -c ../include/process.c
+ar rcs libprocess.a process.o
+
+ar rcs libraffer.a error.o str.o file.o process.o
